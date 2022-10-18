664c3829480afb7937257ab58b82810c00aa17c9
ysfish <ysfish@users.noreply.github.com>
2022-10-17 23:16:08 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,31 @@
+# Configuration file
+
+PATH = "/home/brian/kraken_watch/"
+CONTRACT_ADDRESS = "0xC17c30e98541188614dF99239cABD40280810cA3"
+CROSS_CHAIN_BUYBACK_ADDRESS = "0x3BB730E6f651007F2963B627C0D992CFC120a352"
+BURN_ADDRESS = "0x000000000000000000000000000000000000dead"
+NFT_RISE_ADDRESS = "0x23cd2e6b283754fd2340a75732f9ddbb5d11807e"
+
+CHAINS = ["bsc", "eth", "poly", "ftm", "avax"]
+CHAINS1 = ["bsc", "eth", "poly"]
+CHAINS2 = ["ftm", "avax"]
+DECIMALS = 18
+
+PANCAKESWAP = "0x10ed43c718714eb63d5aa57b78b54704e256024e"
+UNISWAP = "0x7a250d5630b4cf539739df2c5dacb4c659f2488d"
+QUICKSWAP = "0xa5e0829caced8ffdd4de3c43696c57f7d7a678ff"
+SPOOKYSWAP = "0xde62a6cdd8d5a3988495317cffac9f3fed299383"
+TRADERJOE = "0x5472e98d22b0fb7ec5c3e360788b8700419370b5"
+
+EVERRISE_STATS = "https://everrise.azurewebsites.net/stats"
+API_URL_TXINTERNAL = "api?module=account&action=txlistinternal&address="
+API_URL_TXLIST = "api?module=account&action=txlist&address="
+API_URL_BSC = "https://api.bscscan.com/"
+API_URL_ETH = "https://api.etherscan.io/"
+API_URL_POLY = "https://api.polygonscan.com/"
+API_URL_FTM = "https://api.ftmscan.com/"
+API_URL_AVAX = "https://api.snowtrace.io/"
+API_URL_ENDBLOCK = "&endblock=999999999&sort=asc&apikey="
+
+TELEGRAM_MUTE = False
+TWITTER_MUTE = False
---
+++ kraken_watch.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+
+import telegram
+import tweepy
+import mariadb
+import requests
+from credentials import *
+from datetime import datetime
+from config import *
+
+def main():
+  conn = connectToMariaDB()
+  cur = conn.cursor()
+  for CHAIN in CHAINS1:
+    print("Current Blockchain: ", CHAIN.upper())
+    cur.execute(f"SELECT BlockNumber FROM {CHAIN}_kraken_watch;")
+    start_block = cur.fetchall()[-1][0] + 1
+    if(CHAIN == "bsc"):
+      CHAIN2 = "bsc"
+      CHAIN3 = "BNB Chain"
+      NATIVE = "BNB"
+      router = PANCAKESWAP
+      request_url = API_URL_BSC + API_URL_TXINTERNAL + CONTRACT_ADDRESS + "&startblock=" + str(start_block) + API_URL_ENDBLOCK + BSC_API_KEY
+      telegram_hash_source = "\n🐋🐋 TX Hash: [BSCScan](https://bscscan.com/tx/"
+      twitter_hash_source = "\n🐋 TX Hash: https://bscscan.com/tx/"
+    if(CHAIN == "eth"):
+      CHAIN2 = "eth"
+      CHAIN3 = "Ethereum"
+      NATIVE = "ETH"
+      router = UNISWAP
+      request_url = API_URL_ETH + API_URL_TXINTERNAL + CONTRACT_ADDRESS + "&startblock=" + str(start_block) + API_URL_ENDBLOCK + ETH_API_KEY
+      telegram_hash_source = "\n🐋🐋 TX Hash: [EtherScan](https://etherscan.io/tx/"
+      twitter_hash_source = "\n🐋 TX Hash: https://etherscan.io/tx/"
+    if(CHAIN == "poly"):
+      CHAIN2 = "polygon"
+      CHAIN3 = "Polygon"
+      NATIVE = "MATIC"
+      router = QUICKSWAP
+      request_url = API_URL_POLY + API_URL_TXINTERNAL + CONTRACT_ADDRESS + "&startblock=" + str(start_block) + API_URL_ENDBLOCK + POLY_API_KEY
+      telegram_hash_source = "\n🐋🐋 TX Hash: [PolygonScan](https://polygonscan.com/tx/"
+      twitter_hash_source = "\n🐋 TX Hash: [PolygonScan](https://polygonscan.com/tx/"
+    bot = connectToTelegram()
+    api = connectToTwitter()
+    current_time = datetime.utcnow()
+    print("Current Time:        ",current_time)
+    print("Getting EverRise data...")
+    everrise_current_data = requests.get(EVERRISE_STATS).json()[CHAIN]["current"]
+    everrise_staked = everrise_current_data["staked"]
+    everrise_multiplier = everrise_current_data["aveMultiplier"]
+    current_token_price = everrise_current_data["tokenPriceStable"]
+    current_coin_price = everrise_current_data["coinPriceStable"]
+    internal_txns = requests.get(request_url).json()["result"]
+    for item in internal_txns:
+      if item['from'] == CONTRACT_ADDRESS.lower():
+        if item['to'] == str(router).lower():
+          kraken_found = 1
+        else:
+          kraken_found = 0
+      else:
+        kraken_found = 0
+      if kraken_found == 1:
+        native_value = int(item['value'])/1000000000000000000
+        current_rewards = float(native_value) * float(current_coin_price) / float(current_token_price)
+        staker_percentage = (float(current_rewards) * 36) / (float(everrise_staked) * float(everrise_multiplier)) * 100
+        kaboom_datetime = datetime.utcfromtimestamp(int(item['timeStamp']))
+        print("Date/Time of Kaboom:   ",kaboom_datetime)
+        print("TimeStamp:             ",item['timeStamp'])
+        print("Block Number:          ",item['blockNumber'])
+        print(f"{NATIVE} Value:       ",native_value)
+        print("EverRise Value:        ",current_rewards)
+        print("Transaction Hash:      ",item['hash'])
+        cur.execute(f"INSERT INTO {CHAIN}_kraken_watch (TimeStamp,BlockNumber,{NATIVE}Value,TransactionHash) VALUES (?, ?, ?, ?)", (item['timeStamp'],item['blockNumber'],native_value,item['hash']))
+        conn.commit()
+        cur.execute(f"INSERT INTO {CHAIN}_kraken_rewards (TimeStamp,{NATIVE}Rewards,{NATIVE}Performance) VALUES (?, ?, ?)", (item['timeStamp'],current_rewards,staker_percentage))
+        conn.commit()
+        telegram_message = f"🐋🐋 *{CHAIN.upper()} Kraken Sighting:*\n🐋🐋 *Date/Time of Kaboom:* " + str(kaboom_datetime) + "\n🐋🐋 *Block Number:*               " + str(item['blockNumber']) + f"\n🐋🐋 *{NATIVE} Value:*                      " + str(native_value) + "\n🐋🐋 *RISE Rewards Distributed:*      " + str("{:,}".format(round(current_rewards,2))) + " RISE\n🐋🐋 *36x Stake Performance:*          " + str(round(staker_percentage,6)) + "%"
+        bot.send_message(chat_id=CHAT_ID, text=telegram_message, parse_mode=telegram.ParseMode.MARKDOWN)
+        twitter_message = f"🐋 #EverRise $RISE\n\n🐋 {CHAIN.upper()} Kraken:\n\nTime: " + str(kaboom_datetime) + f"\n🐋 {NATIVE}:  " + str(native_value) + "\n🐋 RISE: " + str("{:,}".format(round(current_rewards,2))) + "\n🐋 36x Staker Earned: " + str(round(staker_percentage, 6)) + "%"
+        api.update_status(status=twitter_message)
+  for CHAIN in CHAINS2:
+    print("Current Blockchain: ", CHAIN.upper())
+    cur.execute(f"SELECT BlockNumber FROM {CHAIN}_kraken_watch;")
+    start_block = cur.fetchall()[-1][0] + 1
+    if(CHAIN == "ftm"):
+      CHAIN2 = "fantom"
+      CHAIN3 = "Fantom"
+      NATIVE = "FTM"
+      router = SPOOKYSWAP
+      request_url1 = API_URL_FTM + API_URL_TXLIST + CROSS_CHAIN_BUYBACK_ADDRESS + "&startblock=" + str(start_block) + API_URL_ENDBLOCK + FTM_API_KEY
+      telegram_hash_source = "\n🐋🐋 TX Hash: [FTMScan](https://ftmscan.com/tx/"
+      twitter_hash_source = "\n🐋 TX Hash: [FTMScan](https://ftmscan.com/tx/"
+    if(CHAIN == "avax"):
+      CHAIN2 = "avalanche"
+      CHAIN3 = "Avalanche"
+      NATIVE = "AVAX"
+      router = TRADERJOE
+      request_url1 = API_URL_AVAX + API_URL_TXLIST + CROSS_CHAIN_BUYBACK_ADDRESS + "&startblock=" + str(start_block) + API_URL_ENDBLOCK + AVAX_API_KEY
+      telegram_hash_source = "\n🐋🐋 TX Hash: [SnowTrace](https://snowtrace.io/tx/"
+      twitter_hash_source = "\n🐋 TX Hash: [SnowTrace](https://snowtrace.io/tx/"
+    bot = connectToTelegram()
+    api = connectToTwitter()
+    current_time = datetime.utcnow()
+    print("Current Time:        ",current_time)
+    print("Getting EverRise data...")
+    everrise_current_data = requests.get(EVERRISE_STATS).json()[CHAIN]["current"]
+    everrise_staked = everrise_current_data["staked"]
+    everrise_multiplier = everrise_current_data["aveMultiplier"]
+    current_token_price = everrise_current_data["tokenPriceStable"]
+    current_coin_price = everrise_current_data["coinPriceStable"]
+    txns_result = requests.get(request_url1).json()["result"]
+    for item in txns_result:
+      if item['from'] == CROSS_CHAIN_BUYBACK_ADDRESS.lower():
+        kraken_found = 1
+      else:
+        kraken_found = 0
+      if kraken_found == 1:
+        txn_hash = item['hash']
+        print("Kraken Found!")
+        if(CHAIN == "ftm"):
+          request_url2 = "https://api.ftmscan.com/api?module=account&action=txlistinternal&txhash=" + txn_hash + "&apikey=" + FTM_API_KEY
+        if(CHAIN == "avax"):
+          request_url2 = "https://api.snowtrace.io/api?module=account&action=txlistinternal&txhash=" + txn_hash + "&apikey=" + AVAX_API_KEY
+        internal_txns_result = requests.get(request_url2).json()["result"]
+        if internal_txns_result == []:
+          query = f"SELECT {NATIVE}Value FROM {CHAIN}_kraken_watch;"
+          cur.execute(query)
+          native_value = cur.fetchall()[-1][0]
+        else:  
+          native_value = int(internal_txns_result[0]['value'])/(10 ** DECIMALS)
+        current_rewards = float(native_value) * float(current_coin_price) / float(current_token_price)
+        staker_percentage = (float(current_rewards) * 36) / (float(everrise_staked) * float(everrise_multiplier)) * 100
+        kaboom_datetime = datetime.utcfromtimestamp(int(item['timeStamp']))
+        print('Date/Time of Kaboom: ',kaboom_datetime)
+        print('Block Number:        ',item['blockNumber'])
+        print(f"{NATIVE} Value:          ",native_value)
+        print("Transaction Hash:      ",item['hash'])
+        cur.execute(f"INSERT INTO {CHAIN}_kraken_watch (DateTime,TimeStamp,BlockNumber,{NATIVE}Value,TransactionHash) VALUES (?, ?, ?, ?, ?)", (kaboom_datetime,item['timeStamp'],item['blockNumber'],native_value,item['hash']))
+        conn.commit()
+        cur.execute(f"INSERT INTO {CHAIN}_kraken_rewards (DateTime,TimeStamp,{NATIVE}Rewards,{NATIVE}Performance) VALUES (?, ?, ?, ?)", (kaboom_datetime,item['timeStamp'],current_rewards,staker_percentage))
+        conn.commit()
+        telegram_message = f"🐋🐋 *{CHAIN.upper()} Kraken Sighting:*\n*🐋🐋 Date/Time of Kaboom:* " + str(kaboom_datetime) + "\n🐋🐋 *Block Number:*               " + str(item['blockNumber']) + f"\n🐋🐋 *{NATIVE} Value:*                      " + str(native_value) + "\n🐋🐋 *RISE Rewards Distributed:*      " + str("{:,}".format(round(current_rewards,2))) + " RISE\n🐋🐋 *36x Stake Performance:*          " + str(round(staker_percentage,6)) + "%"
+        bot.send_message(chat_id=CHAT_ID, text=telegram_message, parse_mode=telegram.ParseMode.MARKDOWN)
+        twitter_message = f"🐋 #EverRise $RISE\n\n🐋 {CHAIN.upper()} Kraken:\n\n🐋 Time: " + str(kaboom_datetime) + f"\n🐋 {NATIVE}: " + str(native_value) + "\n🐋 RISE: " + str("{:,}".format(round(current_rewards,2))) + "\n🐋 36x Staker Earned: " + str(round(staker_percentage, 6)) + "%"
+        api.update_status(status=twitter_message)
+
+def connectToTwitter():
+    print("Connecting to Twitter...")
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    print("Success!")
+    return api
+
+def connectToTelegram():
+    print("Connecting to Telegram Bot...")
+    try:
+      bot = telegram.Bot(token=TELEGRAM_API_KEY)
+      print("Success!")
+    except telegram.Error as e:
+      print(f"Error connecting to Telegram: {e}")
+    return bot
+
+def connectToMariaDB():
+    print("Connecting to  MariaDB Instance...")
+    try:
+      conn = mariadb.connect(
+        user=MARIADB_USER,
+        password=MARIADB_PW,
+        host=MARIADB_HOST,
+        port=3306,
+        database=MARIADB_DB
+      )
+      print("Success!")
+    except mariadb.Error as e:
+      print(f"Error connecting to MariaDB Platform: {e}")
+    return conn
+
+if __name__ == "__main__":
+  main()
